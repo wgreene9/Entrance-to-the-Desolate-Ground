0062729557a45f5c20b028e5ef81238f370c1983
wgreene9 <122186761+wgreene9@users.noreply.github.com>
2026-01-08 21:41:45 -0500

    Version 1.293

---
+++ common/defines/gme.lua
@@ -0,0 +1,25 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NDiplomacy.GREAT_PROJECT_WARSCORE_COST_MODIFIER = 0.02
+
+NDefines.NDiplomacy.MAX_FREE_CITIES = 12
+NDefines.NDiplomacy.IMPERIAL_CITY_IA = 0.010						-- Monthly per imperial free city
+
+NDefines.NDiplomacy.IMPERIAL_AUTHORITY_FROM_PRINCES = 0.075
+
+NDefines.NReligion.MAX_CHRISTIAN_RELIGIOUS_CENTERS = 5
+
+NDefines.NReligion.MAX_RELIGIOUS_CENTER_SPREAD_DISTANCE = 2000.0
+
+NDefines.NMilitary.BASE_MP_TO_MANPOWER = 0.2		-- 250 men per manpower
+
+NDefines.NCountry.HRE_MAX_RANK = 2
+
+NDefines.NAI.FORT_BUDGET_FRACTION = 0.2
+
+NDefines.NAI.DEVELOPMENT_CAP_BASE = 4	-- AI will not develop provinces that have more development than this or DEVELOPMENT_CAP_MULT*original development (whichever is bigger)
+NDefines.NAI.DEVELOPMENT_CAP_MULT = 0.2
+
+NDefines.NGame.MAX_GOVERNMENT_REFORMS_PER_TIER = 24
